55b189d5cc749789f71163fd565dafbbc3892cd6
ArkPal <arkadiusz.palczak@gmail.com>
2022-12-10 18:17:20 +0100

    Contact Us form tests

---
+++ cypress/e2e/contact_us.cy.js
@@ -0,0 +1,51 @@
+///<reference types="cypress"/>
+
+const tests = require("../fixtures/contact_us.json");
+
+describe("Newsletter subscribe", () => {
+    beforeEach(() => {
+        cy.visit("/contact-us");
+    });
+
+    tests.forEach((test) => {
+        it(test.description, () => {
+            cy.xpath(
+                '//div[@id="body-section"]//form//input[@name="contact_name"]'
+            )
+                .scrollIntoView()
+                .clear()
+                .type(test.name)
+                .should("have.attr", "required");
+            cy.xpath(
+                '//div[@id="body-section"]//form//input[@name="contact_email"]'
+            )
+                .scrollIntoView()
+                .clear()
+                .type(test.email)
+                .should("have.attr", "required");
+            cy.xpath(
+                '//div[@id="body-section"]//form//input[@name="contact_subject"]'
+            )
+                .scrollIntoView()
+                .clear()
+                .type(test.subject)
+                .should("have.attr", "required");
+            if (test.message) {
+                cy.xpath(
+                    '//div[@id="body-section"]//form//textarea[@name="contact_message"]'
+                )
+                    .scrollIntoView()
+                    .clear()
+                    .type(test.message);
+            }
+            cy.xpath(
+                '//div[@id="message-contact"]/../..//*[@name="submit_contact"]'
+            )
+                .scrollIntoView()
+                .click();
+            cy.xpath('//div[@id="body-section"]//form/div[1]/div[1]')
+                .should("have.class", test.expectedAlertStyle)
+                .and("contain", test.expectedMessage);
+        });
+    });
+});
